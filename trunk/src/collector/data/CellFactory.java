51e63c1b95121863f93e2c5a73c8ac365be0b538
snowgoon88 <snowgoon88@4ebd8e13-c52d-0410-bdff-eed6d8b617ac>
2007-08-21 12:11:23 +0000

    CellFactory and DataHandler modified so as to correctly read XML files. EnregistrementFactory is just correctly indented.

--- trunk/src/collector/data/CellFactory.java
+++ trunk/src/collector/data/CellFactory.java
@@ -13,163 +13,179 @@ import org.xml.sax.Attributes;
 
 public class CellFactory 
 {
-    /** Cell can be a CellStr */
-    CellStr myCellStr;
-    /** Cell can be a CellInt */
-    CellInt myCellInt;
-
-    /** A current Label */
-    String myLabel;
-    /** A current Type */
-    String myType;
-    /** Inherit ? */
-    Boolean inherit;
-
-    /** Possible parent Enregistrement */
-    Enregistrement parentData;
-
-    /** boolean flag for Str*/
-    boolean inCellStr = false;
-    /** boolean flag for Int */
-    boolean inCellInt = false;
-
-    /**
-     * Creation.
-     *
-     * @param p_parentData Possible parent Enregistrement for finding Cell's parent.
-     */
-    public CellFactory( Enregistrement p_parentData ) 
-    {
-	//logger = Logger.getLogger(CellFactory.class);
-	logger = Logger.getLogger( "collector.data.XML.CellFactory");
-	parentData = p_parentData;
-    }
-
-    /**
-     * classic.
-     *
-     * Output format:<br>
-     * 
-     */
-    public String toString()
-    {
-	StringBuffer str = new StringBuffer();
-
-	str.append("\n");
-
-	return str.toString();
-    }
-
-    /**
-     * Decode startElement from XML.
-     *
-     * @param p_field Supposed field linked to the cell. It will not be checked.
-     */
-    public void startElement( String qName,
-			      Attributes attributes,
-			      Field p_field)
-    {
-	logger.debug("start element " + qName);
-	// look for the type...
-	if (attributes != null) {
-	    myType = attributes.getValue( Element.ATT_ELEMENT_TYPE );
-	    logger.debug("  of type=" + myType );
-	    
-	    inherit = new Boolean( attributes.getValue( Element.ATT_ELEMENT_INHERIT ));
-	    logger.debug("  inherit=" + inherit );
-
-	    // creating the right Cell
-	    if( myType.equals( Element.typeString )) {
-		logger.debug("Create CellStr");
-		myCellStr = new CellStr( p_field );
-
-		//check that possible parent is the good one if needed
-		if( parentData != null ) {
-		    if( p_field.hasParent() ) {
-			Field tmpField = (Field) p_field.getParent();
-			int indexParent = tmpField.index;
-			myCellStr.setParent( parentData.data[indexParent] );
-			// only if with a parent.
-			myCellStr.setInherit( inherit.booleanValue() );
-		    }
-		}
-		inCellStr = true;
-	    }
-	    else if( myType.equals( Element.typeInt )) {
-		logger.debug("Create CellInt");
-		myCellInt = new CellInt( p_field );
-
-		//check that possible parent is the good one if needed
-		if( parentData != null ) {
-		    if( p_field.hasParent() ) {
-			Field tmpField = (Field) p_field.getParent();
-			int indexParent = tmpField.index;
-			myCellInt.setParent( parentData.data[indexParent] );
-			// only if with a parent.
-			myCellInt.setInherit( inherit.booleanValue() );
-		    }
-		}
-		inCellInt = true;
-	    }
-	    else {
-		logger.warn( "Unknwon type : " + myType );
-	    }
-	}
-    }
-    /**
-     * Decode EndElement from XML.
-     * 
-     * @return A new Cell created with data read
-     */
-    public Element endElement(String qName)
-    {
-	logger.debug("end element " + qName);
-	logger.debug("with myType = " + myType );
-
-	// ElementStr
-	if( myType.equals( Element.typeString ) ) {
-	    logger.debug("Send CellStr");
-	    inCellStr = false;
-	    return myCellStr;
-	}
-	else if( myType.equals( Element.typeInt ) ) {
-	    logger.debug("Send CellInt");
-	    inCellInt = false;
-	    return myCellInt;
-	}
-	else {
-	    logger.warn("Unknown type = " + myType );
-	}
-	return null;
-    }
-    /**
-     * Decode character from XML
-     */
-    public void characters(char[] ch, int start, int length) 
-    {
-	logger.debug("characters: [" + new String(ch, start, length) +"]");
 	
-	// Cell.data
-	if ( inCellStr ){
-	    logger.debug("Set myCellStr.data");
-	    myCellStr.setData( new String(ch, start, length ) );
+	/** A temporary buffer */
+	StringBuffer charBuffer;
+	
+	/** Cell can be a CellStr */
+	CellStr myCellStr;
+	/** Cell can be a CellInt */
+	CellInt myCellInt;
+
+	/** A current Label */
+	String myLabel;
+	/** A current Type */
+	String myType;
+	/** Inherit ? */
+	Boolean inherit;
+
+	/** Possible parent Enregistrement */
+	Enregistrement parentData;
+
+	/** boolean flag for Str*/
+	boolean inCellStr = false;
+	/** boolean flag for Int */
+	boolean inCellInt = false;
+
+	/**
+	 * Creation.
+	 *
+	 * @param p_parentData Possible parent Enregistrement for finding Cell's parent.
+	 */
+	public CellFactory( Enregistrement p_parentData ) 
+	{
+		//logger = Logger.getLogger(CellFactory.class);
+		logger = Logger.getLogger( "collector.data.XML.CellFactory");
+		parentData = p_parentData;
+		charBuffer = new StringBuffer("");
 	}
-	else if ( inCellInt ) {
-	    logger.debug("Set myCellInt.data");
-	    Integer dummy = new Integer( new String(ch, start, length ) );
-	    myCellInt.setData( dummy.intValue() );
+
+	/**
+	 * classic.
+	 *
+	 * Output format:<br>
+	 * 
+	 */
+	public String toString()
+	{
+		StringBuffer str = new StringBuffer();
+
+		str.append("\n");
+
+		return str.toString();
 	}
-	else {
-	    logger.debug("Doing nothing with data");
+
+	/**
+	 * Decode startElement from XML.
+	 *
+	 * @param p_field Supposed field linked to the cell. It will not be checked.
+	 */
+	public void startElement( String qName,
+			Attributes attributes,
+			Field p_field)
+	{
+		logger.debug("start element " + qName);
+		// look for the type...
+		if (attributes != null) {
+			myType = attributes.getValue( Element.ATT_ELEMENT_TYPE );
+			logger.debug("  of type=" + myType );
+
+			inherit = new Boolean( attributes.getValue( Element.ATT_ELEMENT_INHERIT ));
+			logger.debug("  inherit=" + inherit );
+
+			// creating the right Cell
+			if( myType.equals( Element.typeString )) {
+				logger.debug("Create CellStr");
+				myCellStr = new CellStr( p_field );
+
+				//check that possible parent is the good one if needed
+				if( parentData != null ) {
+					if( p_field.hasParent() ) {
+						Field tmpField = (Field) p_field.getParent();
+						int indexParent = tmpField.index;
+						myCellStr.setParent( parentData.data[indexParent] );
+						// only if with a parent.
+						myCellStr.setInherit( inherit.booleanValue() );
+					}
+				}
+				inCellStr = true;
+			}
+			else if( myType.equals( Element.typeInt )) {
+				logger.debug("Create CellInt");
+				myCellInt = new CellInt( p_field );
+
+				//check that possible parent is the good one if needed
+				if( parentData != null ) {
+					if( p_field.hasParent() ) {
+						Field tmpField = (Field) p_field.getParent();
+						int indexParent = tmpField.index;
+						myCellInt.setParent( parentData.data[indexParent] );
+						// only if with a parent.
+						myCellInt.setInherit( inherit.booleanValue() );
+					}
+				}
+				inCellInt = true;
+			}
+			else {
+				logger.warn( "Unknwon type : " + myType );
+			}
+		}
 	}
-    }
-    
-    
-    // ---------- a Private Logger ---------------------
-    private Logger logger;
-    // --------------------------------------------------
+	/**
+	 * Decode EndElement from XML.
+	 * 
+	 * @return A new Cell created with data read
+	 */
+	public Element endElement(String qName)
+	{
+		logger.debug("end element " + qName);
+		logger.debug("with myType = " + myType );
+
+		// CellStr
+		if( myType.equals( Element.typeString ) ) {
+			logger.debug("Set myCellStr.data");
+			myCellStr.setData( charBuffer.toString());
+			logger.debug("Send CellStr");
+			inCellStr = false;
+			charBuffer.delete( 0, charBuffer.length());
+			return myCellStr;
+		}
+		// CellInt
+		else if( myType.equals( Element.typeInt ) ) {
+			logger.debug("Set myCellInt.data");
+			Integer dummy = new Integer( charBuffer.toString() );
+			myCellInt.setData( dummy.intValue() );
+			logger.debug("Send CellInt");
+			inCellInt = false;
+			charBuffer.delete( 0, charBuffer.length());
+			return myCellInt;
+		}
+		else {
+			logger.warn("Unknown type = " + myType );
+		}
+		charBuffer.delete( 0, charBuffer.length());
+		return null;
+	}
+	/**
+	 * Decode character from XML
+	 */
+	public void characters(char[] ch, int start, int length) 
+	{
+		logger.debug("characters: [" + new String(ch, start, length) +"]");
+		charBuffer.append( ch, start, length);
+		logger.debug("charBuffer: [" + charBuffer.toString()+"]");
+
+		// Cell.data
+//		if ( inCellStr ){
+//			logger.debug("Set myCellStr.data");
+//			myCellStr.setData( new String(ch, start, length ) );
+//		}
+//		else if ( inCellInt ) {
+//			logger.debug("Set myCellInt.data");
+//			Integer dummy = new Integer( new String(ch, start, length ) );
+//			myCellInt.setData( dummy.intValue() );
+//		}
+//		else {
+//			logger.debug("Doing nothing with data");
+//		}
+	}
+
+
+	// ---------- a Private Logger ---------------------
+	private Logger logger;
+	// --------------------------------------------------
 } // CellFactory
-    
+
 
 
 
--- trunk/src/collector/data/DataContentHandler.java
+++ trunk/src/collector/data/DataContentHandler.java
@@ -15,272 +15,272 @@ import org.xml.sax.helpers.DefaultHandler;
 public class DataContentHandler extends DefaultHandler
 {
 
-    /**
-     * The data that will be read from message and retrieved through
-     * <code>getData()</code>.
-     */
-    Table myData = null;
-    Header myHeader = null;
-    String myLabel;
-    String parent;
+	/**
+	 * The data that will be read from message and retrieved through
+	 * <code>getData()</code>.
+	 */
+	Table myData = null;
+	Header myHeader = null;
+	String myLabel;
+	String parent;
 
-    /** A possible parent Table */
-    Table parentTable = null;
+	/** A possible parent Table */
+	Table parentTable = null;
 
-    /** Need a EnregistrementFactory for parsing */
-    EnregistrementFactory myEnregistrementFactory;
-    /** Need a HeaderFactory for parsing */
-    HeaderFactory myHeaderFactory;
-    /** Can use a DataFactory for the Parent */
-    DataContentHandler myParentFactory;
+	/** Need a EnregistrementFactory for parsing */
+	EnregistrementFactory myEnregistrementFactory;
+	/** Need a HeaderFactory for parsing */
+	HeaderFactory myHeaderFactory;
+	/** Can use a DataFactory for the Parent */
+	DataContentHandler myParentFactory;
 
-    /** are we reading a Table ? */
-    boolean inTable = false;
-    /** are we reading a Enregistrement ? */
-    boolean inEnregistrement = false;
-    /** are we reading a Header ? */
-    boolean inHeader = false;
-    /** are we reading a label ? */
-    boolean inLabel = false;
-    /** are we reading a parent ? */
-    boolean inParent = false;
-    
+	/** are we reading a Table ? */
+	boolean inTable = false;
+	/** are we reading a Enregistrement ? */
+	boolean inEnregistrement = false;
+	/** are we reading a Header ? */
+	boolean inHeader = false;
+	/** are we reading a label ? */
+	boolean inLabel = false;
+	/** are we reading a parent ? */
+	boolean inParent = false;
 
 
-    /**
-     * Creation
-     */
-    public DataContentHandler() 
-    {
-	logger = Logger.getLogger( "collector.data.XML.DataContentHandler" );
-	parentTable = null;
 
-	myHeaderFactory = null;
-	myEnregistrementFactory = null;
-	myParentFactory = null;
+	/**
+	 * Creation
+	 */
+	public DataContentHandler() 
+	{
+		logger = Logger.getLogger( "collector.data.XML.DataContentHandler" );
+		parentTable = null;
 
-    }
+		myHeaderFactory = null;
+		myEnregistrementFactory = null;
+		myParentFactory = null;
 
-    /**
-     * classic.
-     *
-     * Output format:<br>
-     * 
-     */
-    public String toString()
-    {
-	StringBuffer str = new StringBuffer();
+	}
 
-	str.append("DataContentHandler\n");
+	/**
+	 * classic.
+	 *
+	 * Output format:<br>
+	 * 
+	 */
+	public String toString()
+	{
+		StringBuffer str = new StringBuffer();
 
-	return str.toString();
-    }
+		str.append("DataContentHandler\n");
 
-    /**
-     * Return an Element right now.
-     */
-    public Element getData()
-    {
-	return myData;
-    }
+		return str.toString();
+	}
 
-    /**
-     * classic.
-     *
-     * @see org.xml.sax.helpers.DefaultHandler
-     */
-    public void startDocument() 
-    {
-	logger.debug( "----- startDocument -----" );
-    }
+	/**
+	 * Return an Element right now.
+	 */
+	public Element getData()
+	{
+		return myData;
+	}
 
-    /**
-     * classic.
-     *
-     * @see org.xml.sax.helpers.DefaultHandler
-     */
-    public void endDocument() 
-    {
-	logger.debug( "----- endDocument -----");
-    }
+	/**
+	 * classic.
+	 *
+	 * @see org.xml.sax.helpers.DefaultHandler
+	 */
+	public void startDocument() 
+	{
+		logger.debug( "----- startDocument -----" );
+	}
 
-    /**
-     * Call to Factories.
-     *
-     * @see org.xml.sax.helpers.DefaultHandler
-     */
-    public void startElement(java.lang.String uri, 
-			     java.lang.String localName, 
-			     java.lang.String qName,
-			     Attributes attributes)
-    {
-	logger.debug("start element " + qName);
-	
-	if( inTable ) {
+	/**
+	 * classic.
+	 *
+	 * @see org.xml.sax.helpers.DefaultHandler
+	 */
+	public void endDocument() 
+	{
+		logger.debug( "----- endDocument -----");
+	}
 
-	    // in Parent
-	    if( inParent ) {
-		myParentFactory.startElement( uri, localName, qName, attributes );
-	    }
-	    // in Header
-	    else if( inHeader ) {
-		myHeaderFactory.startElement( qName, attributes );
-	    }
-	    // a Header ?
-	    else if( qName.equals( Element.TAG_ELEMENT_HEADER )) {
-		logger.debug("Entering Header");
-		inHeader = true;
-		
-		// is there a parentTable
-		if( parentTable != null ) {
-		    myHeaderFactory = new HeaderFactory( parentTable.myHeader );
-		    myEnregistrementFactory = new EnregistrementFactory( parentTable );
+	/**
+	 * Call to Factories.
+	 *
+	 * @see org.xml.sax.helpers.DefaultHandler
+	 */
+	public void startElement(java.lang.String uri, 
+			java.lang.String localName, 
+			java.lang.String qName,
+			Attributes attributes)
+	{
+		logger.debug("start element " + qName);
+
+		if( inTable ) {
+
+			// in Parent
+			if( inParent ) {
+				myParentFactory.startElement( uri, localName, qName, attributes );
+			}
+			// in Header
+			else if( inHeader ) {
+				myHeaderFactory.startElement( qName, attributes );
+			}
+			// a Header ?
+			else if( qName.equals( Element.TAG_ELEMENT_HEADER )) {
+				logger.debug("Entering Header");
+				inHeader = true;
+
+				// is there a parentTable
+				if( parentTable != null ) {
+					myHeaderFactory = new HeaderFactory( parentTable.myHeader );
+					myEnregistrementFactory = new EnregistrementFactory( parentTable );
+				}
+				else {
+					myHeaderFactory = new HeaderFactory( null );
+					myEnregistrementFactory = new EnregistrementFactory( null );
+				}
+
+				myHeaderFactory.startElement( qName, attributes );
+			}
+			else if(inEnregistrement) {
+				myEnregistrementFactory.startElement( qName, attributes, myHeader );
+			}
+			// aN Element
+			else if( qName.equals( Element.TAG_ELEMENT_ENREGISTREMENT )) {
+				inEnregistrement = true;
+				myEnregistrementFactory.startElement( qName, attributes, myHeader );
+			}
+			// a Label
+			else if( qName.equals( Element.TAG_ELEMENT_LABEL )) {
+				logger.debug("Reading Table:label");
+				inLabel = true;
+			}	
+			// a Parent
+			else if( qName.equals( Element.TAG_ELEMENT_PARENT_TABLE )) {
+				logger.debug("Reading Table:parent");
+				inParent = true;
+
+				myParentFactory = new DataContentHandler();
+			}
+			else {
+				logger.warn("Unknown element inTable:" + qName);
+			}
+		}
+		// a Table
+		else if( qName.equals( Element.TAG_ELEMENT_TABLE )) {
+			logger.debug("Entering Table");
+			inTable = true;
+		}
+		// Unknown
+		else {
+			logger.warn("Unknown element !!!");
+		}
+	}
+	/**
+	 * Should create Elements.
+	 *
+	 * @see org.xml.sax.helpers.DefaultHandler
+	 */
+	public void endElement(java.lang.String uri, 
+			java.lang.String localName, 
+			java.lang.String qName) 
+	{
+		logger.debug("end element " + qName);
+
+		// a Table
+		if( qName.equals( Element.TAG_ELEMENT_TABLE )) {
+			logger.debug("Leaving Table");
+			inTable = true;
+		}
+		else if( inTable ) {
+
+			// end of Parent
+			if( qName.equals( Element.TAG_ELEMENT_PARENT_TABLE )) {
+				logger.debug("End Table:parent");
+				inParent = false;
+
+				parentTable = (Table) myParentFactory.getData();
+				myData.setParent( parentTable );
+				logger.debug( "------PARENT-----\n" + parentTable );
+			}
+			// in Parent
+			if( inParent ) {
+				myParentFactory.endElement( uri, localName, qName );
+			}
+			else if( qName.equals( Element.TAG_ELEMENT_HEADER )) {
+				logger.debug("Sending Header");
+				inHeader = false;
+				myHeader = myHeaderFactory.endElement( qName );
+				myData.setHeader( myHeader );
+			}
+			// in Header 
+			else if( inHeader ) {
+				myHeaderFactory.endElement( qName );
+			}
+			// end of Enregistrement
+			else if( qName.equals( Element.TAG_ELEMENT_ENREGISTREMENT )) {
+				logger.debug("Add Enregistrement");
+				// can only be an Enregistrement
+				inEnregistrement = false;
+				Enregistrement tmpEnr = myEnregistrementFactory.endElement( qName );
+				myData.add( tmpEnr, tmpEnr.key );
+			}
+			else if( inEnregistrement ) {
+				myEnregistrementFactory.endElement( qName ) ;
+			}
+			// a Label
+			else if( qName.equals( Element.TAG_ELEMENT_LABEL )) {
+				logger.debug("End Table:label");
+				inLabel = false;
+				logger.debug("Creating Table");
+				myData = new Table( myLabel );
+			}
+		}
+		// unknown
+		else {
+			logger.warn("Unknown element");
+		} 
+	}
+	/**
+	 * Call to Factories.
+	 *
+	 * @see org.xml.sax.helpers.DefaultHandler
+	 */ 
+	public void characters(char[] ch, int start, int length) 
+	{
+		logger.debug("characters: [" + new String(ch, start, length) +"]");
+
+		if (inEnregistrement) {  
+			myEnregistrementFactory.characters( ch, start, length );
+		}
+		else if( inHeader ) {
+			myHeaderFactory.characters( ch, start, length );
+		}
+		else if( inLabel ) {
+			myLabel = new String(ch, start, length );
+		}
+		else if( inParent ) {
+			myParentFactory.characters(ch, start, length );
+		}
+		else if (inTable) {
 		}
 		else {
-		    myHeaderFactory = new HeaderFactory( null );
-		    myEnregistrementFactory = new EnregistrementFactory( null );
+			logger.warn( "### inLabel = " + inLabel );
+			logger.warn( "### inEnregistrement = "+ inEnregistrement );
+			logger.warn( "### inHeader = " + inHeader );
+			logger.warn( "### inParent = " + inParent );
+			logger.warn( "########## Don't know what to do" );
 		}
-		
-		myHeaderFactory.startElement( qName, attributes );
-	    }
-	    else if(inEnregistrement) {
-		myEnregistrementFactory.startElement( qName, attributes, myHeader );
-	    }
-	    // aN Element
-	    else if( qName.equals( Element.TAG_ELEMENT_ENREGISTREMENT )) {
-		inEnregistrement = true;
-		myEnregistrementFactory.startElement( qName, attributes, myHeader );
-	    }
-	    // a Label
-	    else if( qName.equals( Element.TAG_ELEMENT_LABEL )) {
-		logger.debug("Reading Table:label");
-		inLabel = true;
-	    }	
-	    // a Parent
-	    else if( qName.equals( Element.TAG_ELEMENT_PARENT_TABLE )) {
-		logger.debug("Reading Table:parent");
-		inParent = true;
+	}
 
-		myParentFactory = new DataContentHandler();
-	    }
-	    else {
-		logger.warn("Unknown element inTable:" + qName);
-	    }
-	}
-	// a Table
-	else if( qName.equals( Element.TAG_ELEMENT_TABLE )) {
-	    logger.debug("Entering Table");
-	    inTable = true;
-	}
-	// Unknown
-	else {
-	    logger.warn("Unknown element !!!");
-	}
-    }
-    /**
-     * Should create Elements.
-     *
-     * @see org.xml.sax.helpers.DefaultHandler
-     */
-    public void endElement(java.lang.String uri, 
-			   java.lang.String localName, 
-			   java.lang.String qName) 
-    {
-	logger.debug("end element " + qName);
-	
-	// a Table
-	if( qName.equals( Element.TAG_ELEMENT_TABLE )) {
-	    logger.debug("Leaving Table");
-	    inTable = true;
-	}
-	else if( inTable ) {
-
-	    // end of Parent
-	    if( qName.equals( Element.TAG_ELEMENT_PARENT_TABLE )) {
-		logger.debug("End Table:parent");
-		inParent = false;
-
-		parentTable = (Table) myParentFactory.getData();
-		myData.setParent( parentTable );
-		logger.debug( "------PARENT-----\n" + parentTable );
-	    }
-	    // in Parent
-	    if( inParent ) {
-		myParentFactory.endElement( uri, localName, qName );
-	    }
-	    else if( qName.equals( Element.TAG_ELEMENT_HEADER )) {
-		logger.debug("Sending Header");
-		inHeader = false;
-		myHeader = myHeaderFactory.endElement( qName );
-		myData.setHeader( myHeader );
-	    }
-	    // in Header 
-	    else if( inHeader ) {
-		myHeaderFactory.endElement( qName );
-	    }
-	    // end of Enregistrement
-	    else if( qName.equals( Element.TAG_ELEMENT_ENREGISTREMENT )) {
-		logger.debug("Add Enregistrement");
-		// can only be an Enregistrement
-		inEnregistrement = false;
-		Enregistrement tmpEnr = myEnregistrementFactory.endElement( qName );
-		myData.add( tmpEnr, tmpEnr.key );
-	    }
-	    else if( inEnregistrement ) {
-		myEnregistrementFactory.endElement( qName ) ;
-	    }
-	    // a Label
-	    else if( qName.equals( Element.TAG_ELEMENT_LABEL )) {
-		logger.debug("End Table:label");
-		inLabel = false;
-		logger.debug("Creating Table");
-		myData = new Table( myLabel );
-	    }
-	}
-	// unknown
-	else {
-	    logger.warn("Unknown element");
-	} 
-    }
-    /**
-     * Call to Factories.
-     *
-     * @see org.xml.sax.helpers.DefaultHandler
-     */ 
-    public void characters(char[] ch, int start, int length) 
-    {
-	logger.debug("characters: [" + new String(ch, start, length) +"]");
-	
-	if (inEnregistrement) {  
-	    myEnregistrementFactory.characters( ch, start, length );
-	}
-	else if( inHeader ) {
-	    myHeaderFactory.characters( ch, start, length );
-	}
-	else if( inLabel ) {
-	    myLabel = new String(ch, start, length );
-	}
-	else if( inParent ) {
-	    myParentFactory.characters(ch, start, length );
-	}
-	else if (inTable) {
-	}
-	else {
-	    logger.warn( "### inLabel = " + inLabel );
-	    logger.warn( "### inEnregistrement = "+ inEnregistrement );
-	    logger.warn( "### inHeader = " + inHeader );
-	    logger.warn( "### inParent = " + inParent );
-	    logger.warn( "########## Don't know what to do" );
-	}
-    }
-    
-    // ---------- a Private Logger ---------------------
-    private Logger logger;
-    // --------------------------------------------------
+	// ---------- a Private Logger ---------------------
+	private Logger logger;
+	// --------------------------------------------------
 } // DataContentHandler
-    
 
-  
+
+
 
 
 
